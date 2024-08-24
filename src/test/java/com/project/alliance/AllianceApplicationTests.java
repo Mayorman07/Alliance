308cccab8beb2f1b82d381aa2029012b253e6526
Mayorman07 <olajidemayorwa@gmail.com>
2024-08-24 10:28:04 +0100

    initial test commit

---
+++ src/test/java/com/project/alliance/AllianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.alliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
