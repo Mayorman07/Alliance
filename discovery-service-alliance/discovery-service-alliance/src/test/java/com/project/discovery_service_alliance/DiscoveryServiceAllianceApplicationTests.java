e0589cf7366caabbb3463afb18dbcfb834fb30b9
Mayorman07 <olajidemayorwa@gmail.com>
2024-08-29 19:10:29 +0100

    moving all repos

---
+++ discovery-service-alliance/discovery-service-alliance/src/test/java/com/project/discovery_service_alliance/DiscoveryServiceAllianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.discovery_service_alliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceAllianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
